748606a2465e0719b4c5d162d21886fbe3487f5e
Irina14 <ira1786@bk.ru>
2018-11-18 15:29:18 +0700

    Сделан текст.

--- js/stat.js
+++ js/stat.js
@@ -6,6 +6,8 @@ var CLOUD_RADIUS = 50;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var GAP = 10;
+var TEXT_GAP = 30;
+var LINE_HEIGHT = 20;
 
 var renderCloud = function (ctx, x, y, width, height, radius, color) {
   ctx.fillStyle = color;
@@ -26,4 +28,13 @@ var renderCloud = function (ctx, x, y, width, height, radius, color) {
 window.renderStatistics = function (ctx) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, CLOUD_WIDTH, CLOUD_HEIGHT, CLOUD_RADIUS, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT, CLOUD_RADIUS, '#fff');
+
+  ctx.font = '16px PT Mono';
+  ctx.textBaseline = 'hanging';
+  ctx.fillStyle = '#000';
+  var textLines = ['Ура вы победили!', 'Список результатов:'];
+
+  for (var i = 0; i < textLines.length; i++) {
+    ctx.fillText(textLines[i], CLOUD_X + TEXT_GAP, CLOUD_Y + TEXT_GAP + LINE_HEIGHT * i);
+  }
 };
